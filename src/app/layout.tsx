524bee526915c4a6dc8146e59ecc1e56435ef0bb
only1stumpy <rei228322@gmail.com>
2026-03-18 04:01:52 +0200

    feat: add AdSense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ import BgWrapper from "@/components/ui/BgWrapper";
 import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,34 +29,39 @@ const russo = Russo_One({
 export const metadata: Metadata = {
   title: "GD Quiz",
   description:
-    "Test your skills by ranking the hardest levels from most impossible to easiest",
+      "Test your skills by ranking the hardest levels from most impossible to easiest",
   keywords:
-    "gd, geometry dash, quiz, game, levels, impossible, hardest, easiest, demonlist, demon list, global demonlist, global demon list, hardest levels, hardest levels in geometry dash, hardest levels in gd, hardest levels in geometry dash, hardest levels in gd, impossible levels, impossible levels in geometry dash, impossible levels in gd, demon list quiz, global demon list quiz, global demonlist quiz, demonlist quiz, geometry dash quiz, gd quiz, demonlist challenge, global demonlist challenge, demon list challenge, geometry dash challenge, gd challenge, quiz challenge, hardest levels challenge, impossible levels challenge, impossible levels in geometry dash challenge, impossible levels in gd challenge, hardest levels in geometry dash challenge, hardest levels in gd challenge",
+      "gd, geometry dash, quiz, game, levels, impossible, hardest, easiest, demonlist, demon list, global demonlist, global demon list, hardest levels, hardest levels in geometry dash, hardest levels in gd, hardest levels in geometry dash, hardest levels in gd, impossible levels, impossible levels in geometry dash, impossible levels in gd, demon list quiz, global demon list quiz, global demonlist quiz, demonlist quiz, geometry dash quiz, gd quiz, demonlist challenge, global demonlist challenge, demon list challenge, geometry dash challenge, gd challenge, quiz challenge, hardest levels challenge, impossible levels challenge, impossible levels in geometry dash challenge, impossible levels in gd challenge, hardest levels in geometry dash challenge, hardest levels in gd challenge",
   icons: {
     icon: "/favicon.ico",
   },
 };
 
 const MainLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-
   return (
-    <html lang="en">
+      <html lang="en">
       <body
-        className={`${inter.variable} ${orbitron.variable} ${russo.variable} antialiased`}
+          className={`${inter.variable} ${orbitron.variable} ${russo.variable} antialiased`}
       >
-        <SpeedInsights />
-        <BgWrapper />
-        <Header />
-        <main>{children}</main>
-        <Footer />
-        <ToastContainer
-            position="top-center"
-            autoClose={3000}
-            hideProgressBar
-            theme="dark"
-        />
+      <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2548493071724055"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+      />
+      <SpeedInsights />
+      <BgWrapper />
+      <Header />
+      <main>{children}</main>
+      <Footer />
+      <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar
+          theme="dark"
+      />
       </body>
-    </html>
+      </html>
   );
 };
 
